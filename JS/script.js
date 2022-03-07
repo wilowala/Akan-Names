4fc8f62814d004416f23f104997533fa4e5c34e9
William Owalla <william.owalla@student.moringaschool.com>
2022-03-07 15:32:50 +0300

    ft: optimized business logic functionality

--- JS/script.js
+++ JS/script.js
@@ -7,13 +7,18 @@ const error = document.querySelector("#err");
 const errorm = document.querySelector("#errm");
 const errory = document.querySelector("#erry");
 const errorg = document.querySelector("#errg");
+const submit = document.querySelector("#submit");
+const assignName = document.querySelector(".output");
+const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const akanMaleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const akanFemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 // Event Listeners
 
 form.addEventListener("submit", verifyDay);
 form.addEventListener("submit", verifyMonth);
 form.addEventListener("submit", verifyYear);
-form.addEventListener("submit", verifyGender);
+// form.addEventListener("submit", verifyGender);
 form.addEventListener("submit", getAkanName);
 
 var birthDay = [];
@@ -55,10 +60,45 @@ function verifyYear(e) {
     let givenYear = year.value.trim();
     
     if(givenYear.length !== 4) {
-      errory.innerHTML = "Year should be 4 digits: 1900";
+      errory.innerHTML = "Year should be 4 digits eg 1900";
     } else {
         errory.innerHTML = "";
         birthDay.push(givenYear);
     }
 }
+
+function getAkanName() {
+    let recivedDate = date.value; 
+    let recivedMonth = month.value; 
+    let recivedYear = year.value; 
+    // let recivedGender = gender.value; 
+
+    let namingDay = recivedYear + "-" + recivedMonth + "-" + recivedDate;
+    let requiredDay = namingDay.toString();
+    let dayName = new Date(requiredDay);
+    const day = dayName.getDay();
+
+     
+        if (gender.value === "female") {
+            assignName.innerHTML = `${akanFemaleNames[day]}`;
+        }
+        if (gender.value === "male") {
+            assignName.innerHTML = `${akanMaleNames[day]}`;
+        }
+        console.log(gender.value);
+    
+}
+
+// function verifyGender(e) {
+//     e.preventDefault();
+//     let givenGender = gender.value;
+
+//     if(gender.value === "") {
+//       errorg.innerHTML = "Select your gender";
+//     }  else {
+//         errorg.innerHTML = "";
+//         birthDay.push(givenGender);
+//         console.log(birthDay);
+//     }
+//   }
 
