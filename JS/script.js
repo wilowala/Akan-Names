7cbf6c57fe001e9cc57b326837c8b81e64bcfe91
William Owalla <william.owalla@student.moringaschool.com>
2022-03-07 08:03:44 +0300

    ft:[page functionality] added arrays

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+const formData = document.getElementById("submit");
+const displayName = document.querySelector(".output"); // displaying akan names on the desired position
+const userGender = document.querySelectorAll("#gender");
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
